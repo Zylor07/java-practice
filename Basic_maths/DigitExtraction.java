3ea18ced4b2d884cc2a66869e436689cf22d50ae
Pritam Chakraborty <pritam@Pritams-MacBook-Air.local>
2026-01-30 11:05:16 +0530

    Update changes

--- Basic_maths/DigitExtraction.java
+++ Basic_maths/DigitExtraction.java
@@ -1,12 +1,17 @@
 class DigitExtraction{
     public static void digit(int N){
+        int count=0;
         while(N>0){
             int lastDigit=N%10;
+            count++;
             N=N/10;
             
         System.out.println(lastDigit);
 
+
         }
+        System.out.println(count);
+        
     }
     public static void main(String[] args) {
         digit(2278);
